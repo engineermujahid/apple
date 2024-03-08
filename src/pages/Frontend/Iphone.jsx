19c16bdc170ab6c34fce406ebcd3097e25b96e89
hamza-mahboob <57292490+hamza-mahboob@users.noreply.github.com>
2024-03-08 18:39:31 +0500

    Updated Iphone page and added scroll to top component

--- src/pages/Frontend/Iphone.jsx
+++ src/pages/Frontend/Iphone.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import iphone15promedium from "../../assets/images/hero_iphone15pro_medium_2x.jpg";
 import iphoneimg2 from '../../images/iPhone2ndImg.png'
+import iphoneimg3 from '../../images/iPhone3rdImg.png'
+import iphoneimg4 from '../../images/iPhone4thImg.png'
+
 
 export default function Iphone() {
     return (
@@ -32,6 +35,33 @@ export default function Iphone() {
                     <h4 className='text-blue-500 mt-4 font-light text-xl hover:underline hover:cursor-pointer'>Learn more &gt;</h4>
                 </div>
             </div>
+
+            <div className="w-full h-screen relative" style={{ backgroundImage: `url("${iphoneimg3}")`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                <div className='absolute top-1/3 left-1/4 transform -translate-x-1/2 w-1/2 pt-8'>
+                    <div className="flex flex-col items-center">
+                        <h3 className="text-2xl font-medium m-4 align-bottom">iPhone <span className='border-2 text-xs p-1 align-middle border-black rounded-lg'>SE</span></h3>
+                        <h3 className="text-5xl font-semibold m-4 bg-gradient-to-r from-blue-900 to-indigo-600 inline-block text-transparent bg-clip-text text-center">Love the power. <br />Love the price.</h3>
+                        <h3 className="text-xl font-light mt-4 text-center">From $429 or $17.87/mo. for 24 mo.</h3>
+                        <div className='inline-block mt-4'>
+                            <h4 className='text-white bg-blue-600 hover:bg-blue-500 hover:cursor-pointer py-2 px-4 rounded-3xl inline-block'>Buy</h4>
+                        </div>
+                        <h4 className='text-blue-500 mt-4 font-light text-xl hover:underline hover:cursor-pointer'>Learn more &gt;</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-full h-screen relative" style={{ backgroundImage: `url("${iphoneimg4}")`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                <div className='absolute top-1/3 left-1/4 transform -translate-x-1/3 w-1/2 pt-8'>
+                    <div className="flex flex-col items-center">
+                        <h3 className="text-2xl font-medium mt-4 mr-32 text-white">A Guided Tour of</h3>
+                        <h3 className="text-5xl font-semibold m-4 text-white">iPhone 15 &  <br />iPhone 15 Pro.</h3>
+                        <div className='inline-block mt-4 mr-40'>
+                            <h4 className=' bg-gray-200 hover:bg-blue-500 hover:cursor-pointer py-2 px-4 rounded-3xl inline-block'>Watch the film</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 }
--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -4,9 +4,11 @@ import { Route, Routes } from 'react-router-dom'
 import Frontend from "./Frontend"
 import Auth from "./Auth"
 import Dashboard from "./Dashboard"
+import ScrollToTop from '../components/ScrollToTop'
 export default function Index() {
     return (
         <>
+            <ScrollToTop />
             <Routes>
                 <Route path='/*' element={<Frontend />} />
                 <Route path='/auth/*' element={<Auth />} />
